d840f5cb4a776811df286395ddb780c13f85e95d
eddimull <eddimull@gmail.com>
2022-12-17 17:55:20 +0000

    make the dashboard not immediately make the event go away

--- app/Services/UserEventsService.php
+++ app/Services/UserEventsService.php
@@ -10,7 +10,7 @@ class UserEventsService{
     public function getEvents()
     {
         $events = Auth::user()->events
-                ->where('event_time','>',Carbon::now());
+                ->where('event_time','>',Carbon::parse('-72 hour'));
                 // ->where('event_time','<',Carbon::parse('+2 month'));
         return $events;
     }
