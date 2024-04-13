9b87dfb51444fa606a4d833f9ef619840126f1cc
eddimull <eddimull@gmail.com>
2024-04-13 13:33:18 -0500

    fixed header

--- resources/js/Components/Event/Card/Header.vue
+++ resources/js/Components/Event/Card/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="border-b-2 pt-1 pb-3 grid grid-cols-3 items-center">
     <div><card-icon :type="type" /></div>
-    <div class="grid grid-cols-1 items-center">
+    <div class="grid grid-cols-1 items-center text-center">
       <div><span class="font-bold"> {{ name }}</span></div>
     </div>
     <div class="items-center">
-      <span class="float-right decoration-gray-400">{{ parsedDate }}</span>
+      <span class="float-right decoration-gray-400 text-right">{{ parsedDate }}</span>
     </div>
   </div>
 </template>
