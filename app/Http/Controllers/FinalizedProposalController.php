5508254f755afeea6f480b1951fd2e0fbbf76d33
eddimull <eddimull@gmail.com>
2024-08-24 17:26:49 -0500

    got rid of all the compacts. Tests work... but do the tests WORK

--- app/Http/Controllers/FinalizedProposalController.php
+++ app/Http/Controllers/FinalizedProposalController.php
@@ -7,7 +7,7 @@ use App\Models\Proposals;
 use App\Services\FinanceServices;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Inertia\Inertia; 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Str;
 
@@ -15,32 +15,33 @@ class FinalizedProposalController extends Controller
 {
     public function paymentIndex(Proposals $proposal)
     {
+        $proposal->load('payments');  // Assuming attachPayments() is just loading the payments relationship
 
-        $proposal->attachPayments();
-        
-        return Inertia::render('Proposals/ProposalPayments',compact('proposal'));
-    } 
+        return Inertia::render('Proposals/ProposalPayments', [
+            'proposal' => $proposal
+        ]);
+    }
 
     public function submitPayment(Proposals $proposal, Request $request)
     {
         $request->validate([
-            'name'=>'required',
-            'amount'=>'required|Numeric',
-            'paymentDate'=>'required|Date',
+            'name' => 'required',
+            'amount' => 'required|Numeric',
+            'paymentDate' => 'required|Date',
         ]);
-        $payment = (new FinanceServices())->makePayment($proposal,$request->name,$request->amount,$request->paymentDate);
-        
-        $payment->sendReceipt();
-        
+        $payment = (new FinanceServices())->makePayment($proposal, $request->name, $request->amount, $request->paymentDate);
 
-        return back()->with('successMessage','Payment received');
+        $payment->sendReceipt();
+
+
+        return back()->with('successMessage', 'Payment received');
     }
 
-    public function deletePayment(Proposals $proposal,ProposalPayments $payment)
+    public function deletePayment(Proposals $proposal, ProposalPayments $payment)
     {
-        (new FinanceServices())->removePayment($proposal,$payment);
-        
-        return back()->with('successMessage','Payment Removed');
+        (new FinanceServices())->removePayment($proposal, $payment);
+
+        return back()->with('successMessage', 'Payment Removed');
     }
 
     //This is very much like the payment PDF, but downloading the receipt from the site for authorized users
@@ -48,16 +49,19 @@ class FinalizedProposalController extends Controller
     {
         $paymentPDF = $proposal->lastPayment->getPdf();
 
-        return Response::streamDownload(function() use ($paymentPDF){
-           echo $paymentPDF->pdf();
-        },Str::slug($proposal->name . ' Receipt', '_') . '.pdf',
-        [
-            'Content-type'=>'application/pdf'
-        ]);
+        return Response::streamDownload(
+            function () use ($paymentPDF) {
+                echo $paymentPDF->pdf();
+            },
+            Str::slug($proposal->name . ' Receipt', '_') . '.pdf',
+            [
+                'Content-type' => 'application/pdf'
+            ]
+        );
     }
 
     public function paymentPDF(ProposalPayments $payment)
     {
-        return view('pdf.payment',['payment'=>$payment]);
+        return view('pdf.payment', ['payment' => $payment]);
     }
 }
