703aca633d55a58254fca3ca520e39d212657f6a
eddimull <eddimull@gmail.com>
2024-09-10 15:47:09 -0500

    polymorphic payments

--- app/Models/Payments.php
+++ app/Models/Payments.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 
 class Payments extends Model
 {
     use HasFactory;
+
+    protected $table = 'payments';
+
+    protected $fillable = ['name', 'amount', 'date', 'user_id'];
+
+    protected $casts = [
+        'amount' => 'integer',
+        'date' => 'datetime',
+    ];
+
+    public function getformattedPaymentDateAttribute()
+    {
+        return Carbon::parse($this->date)->format('Y-m-d');
+    }
+
+    public function getformattedPaymentAmountAttribute()
+    {
+        return number_format($this->amount / 100, 2);
+    }
+
+    public function payable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
