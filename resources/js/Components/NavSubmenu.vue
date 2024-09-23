ae6cf539a5a50920fbdafd049fd0735b62f427f0
eddimull <eddimull@gmail.com>
2024-09-23 14:23:38 -0500

    async javascript...

--- resources/js/Components/NavSubmenu.vue
+++ resources/js/Components/NavSubmenu.vue
@@ -2,9 +2,10 @@
 import { computed } from 'vue';
 import TabMenu from 'primevue/tabmenu';
 import Link from '@/Components/NavLink.vue';
+import { usePage } from '@inertiajs/inertia-vue3';
 
 
-//find the current route params
+// Find the current route params
 const routeParameters = route().params;
 const props = defineProps({
     routes: {
@@ -14,25 +15,37 @@ const props = defineProps({
 });
 
 const items = computed(() => {
-    return Object.entries(props.routes).map(([name, route]) => ({
+    return Object.entries(props.routes).map(([name, routeInfo]) => ({
         label: name,
-        route: route.uri
+        route: routeInfo.uri,
+        href: route(name, routeParameters,false)
     }));
 });
 
 </script>
 
 <template>
-    <div>
-        <TabMenu :model="items" class="py-2">
-            <template #item="{ item }">
-                <Link :href="route(item.label, routeParameters)" :active="route().current() === item.label" custom
-                    preserve-scroll preserve-state>
-                <a role="menuitem" class="p-menuitem-link">
-                    <span class="p-menuitem-text">{{ item.label }}</span>
-                </a>
-                </Link>
-            </template>
-        </TabMenu>
-    </div>
+  <div>
+    <TabMenu
+      :model="items"
+      class="py-2"
+    >
+      <template #item="{ item }">
+        <Link
+          :href="item.href"
+          :active="$page.url === item.href"
+          custom
+          preserve-scroll
+          preserve-state
+        >
+          <a
+            role="menuitem"
+            class="p-menuitem-link"
+          >
+            <span class="p-menuitem-text">{{ item.label }}</span>
+          </a>
+        </Link>
+      </template>
+    </TabMenu>
+  </div>
 </template>
