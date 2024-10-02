c64d48bfbe7e21c5490005a5902dfc1be2b9cac9
eddimull <eddimull@gmail.com>
2024-10-02 12:46:55 -0500

    confirmed doesn't need edit abilities

--- resources/js/Pages/Bookings/Contract.vue
+++ resources/js/Pages/Bookings/Contract.vue
@@ -3,14 +3,24 @@
     v-if="booking.contract_option === 'none'"
   />
   <ContractExternal
-    v-if="booking.contract_option === 'external'"
+    v-if="booking.contract_option === 'external' && booking.status !== 'confirmed'"
     :booking="booking"
   />
   <ContractEditor
-    v-if="booking.contract_option === 'default'"
+    v-if="booking.contract_option === 'default' && booking.status !== 'confirmed'" 
     :booking="booking"
     :band="band"
   />
+
+  <div v-if="booking.status === 'confirmed'">
+    <p class="text-lg text-center text-gray-600">This booking is confirmed. The contract is no longer editable.</p>
+    <iframe
+        :src="booking.contract.asset_url"
+        width="100%"
+        height="600px"
+        class="border"
+      />
+  </div>
 </template>
   
   <script setup>
