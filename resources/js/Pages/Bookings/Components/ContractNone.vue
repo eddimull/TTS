b4ceb5fe79ee9d345bba3ffad5c479d35d687505
eddimull <eddimull@gmail.com>
2024-10-04 07:38:10 -0500

    automatic confirmation for non-contractable bookings

--- resources/js/Pages/Bookings/Components/ContractNone.vue
+++ resources/js/Pages/Bookings/Components/ContractNone.vue
@@ -1,3 +1,5 @@
 <template>
-  <div>There is no contract associated with this booking.</div>
+      <p class="text-xl text-center text-gray-700 font-semibold bg-blue-100 py-3 px-4 rounded-lg shadow-sm">
+        There is no contract associated with this booking. Contract option is 'none'. 
+      </p>
 </template>
