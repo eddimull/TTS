1868c78d34cb65ebd5b9cfb8c1ea08b3afd69e62
eddimull <eddimull@gmail.com>
2024-11-14 11:21:07 -0600

    Payment emails are now sent with queue and notifications

--- app/Notifications/PaymentReceived.php
+++ app/Notifications/PaymentReceived.php
@@ -2,23 +2,27 @@
 
 namespace App\Notifications;
 
+use App\Models\Payments;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\View;
+use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
-use App\Models\Payments;
 
-class PaymentReceived extends Notification
+class PaymentReceived extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    public Payments $payment;
+    protected Payments $payment;
+    protected $pdf;
+    protected $receiptName;
     /**
      * Create a new notification instance.
      */
     public function __construct(Payments $payment)
     {
         $this->payment = $payment;
+        $this->receiptName = $this->payment->id . ' - Receipt.pdf';
     }
 
 
@@ -32,16 +36,29 @@ class PaymentReceived extends Notification
         return ['mail'];
     }
 
+    private function setupPDF()
+    {
+        $this->pdf = $this->payment->payable->getPaymentPdf();
+    }
+
     /**
      * Get the mail representation of the notification.
      */
     public function toMail(object $notifiable): MailMessage
     {
+        $this->setupPDF();
+
         return (new MailMessage)
-            ->subject('Payment Received for Booking')
-            ->line('A payment of $' . $this->payment->amount . ' has been received.')
-            ->line('Payment Date: ' . $this->payment->formattedPaymentDate)
-            ->line('Booking: ' . $this->payment->payable->name)
-            ->line('Thank you for your payment!');
+            ->subject('Payment Received')
+            ->view('email.payment', [
+                'performance' => $this->payment->payable->name,
+                'amount' => $this->payment->amount,
+                'balance' => $this->payment->payable->amountLeft
+            ])
+            ->attachData(
+                $this->pdf,
+                $this->receiptName,
+                ['mime' => 'application/pdf']
+            );
     }
 }
