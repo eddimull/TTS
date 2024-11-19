ed08fc8dc7fb4eb7f7a550e40b604dcd03d819f1
eddimull <eddimull@gmail.com>
2024-11-19 00:04:55 -0600

    changed the json structure for events

--- resources/js/Components/Event/Card/Body.vue
+++ resources/js/Components/Event/Card/Body.vue
@@ -1,78 +1,104 @@
 <template>
-  <div class="grid grid-cols-1 content-center">
-    <ul>
-      <li class="p-2">
-        Venue: <strong>{{ event.venue_name }}</strong>
-      </li>
-      <li class="p-2">
-        Location: <strong v-if="event.venue_address">{{ event.venue_address }} </strong><strong
-          v-else
-          class="text-red-500"
-        >
-          No address provided
-        </strong>
-      </li>
-      <li class="p-2">
-        Public: <strong>{{ event.additional_data?.public ? 'Yes' : 'No' }}</strong>
-      </li>
-      <li class="p-2">
-        Timeline: 
-        <Times :times="event.additional_data?.times" />
-      </li>
-      <li
-        v-if="event.notes !== null"
-        class="p-2"
-      >
-        Notes: <div
-          class="ml-3 p-3 shadow-lg rounded break-normal content-container bg-gray-100 dark:bg-slate-700"
-          v-html="event.notes"
-        />
-      </li>
-      <li class="p-2">
-        Extra Details:
-        <div
-          class="ml-3 p-3 shadow-lg rounded break-normal bg-gray-100 dark:bg-slate-700">
-          <ul>
-          <li>
-            Outside Event: <strong>{{ event.additional_data?.outside ? 'Yes' : 'No' }}</strong>
-          </li>
-          <li>
-            Lodging Provided: <strong>{{ event.additional_data?.lodging ? 'Yes' : 'No' }}</strong>
-          </li>
-          <li>
-            Backline Provided: <strong>{{ event.additional_data?.backline_provided ? 'Yes' : 'No' }}</strong>
-          </li>
-          <li>
-            Production Needed: <strong>{{ event.additional_data?.production_needed ? 'Yes' : 'No' }}</strong>
-          </li>
+    <div class="grid grid-cols-1 content-center">
+        <ul>
+            <li class="p-2">
+                Venue: <strong>{{ event.venue_name }}</strong>
+            </li>
+            <li class="p-2">
+                Location:
+                <strong v-if="event.venue_address"
+                    >{{ event.venue_address }} </strong
+                ><strong v-else class="text-red-500">
+                    No address provided
+                </strong>
+            </li>
+            <li class="p-2">
+                Public:
+                <strong>{{
+                    event.additional_data?.public ? "Yes" : "No"
+                }}</strong>
+            </li>
+            <li class="p-2">
+                Timeline:
+                <Times :times="event.additional_data?.times" />
+            </li>
+            <li v-if="event.notes !== null" class="p-2">
+                Notes:
+                <div
+                    class="ml-3 p-3 shadow-lg rounded break-normal content-container bg-gray-100 dark:bg-slate-700"
+                    v-html="event.notes"
+                />
+            </li>
+            <li class="p-2">
+                Extra Details:
+                <div
+                    class="ml-3 p-3 shadow-lg rounded break-normal bg-gray-100 dark:bg-slate-700"
+                >
+                    <ul>
+                        <li>
+                            Outside Event:
+                            <strong>{{
+                                event.additional_data?.outside ? "Yes" : "No"
+                            }}</strong>
+                        </li>
+                        <li>
+                            Lodging Provided:
+                            <strong
+                                >{{
+                                    event.additional_data?.lodging?.find(
+                                        (item) =>
+                                            item.title === "Lodging Provided"
+                                    )?.data
+                                        ? "Yes"
+                                        : "No"
+                                }}
+                            </strong>
+                        </li>
+                        <li>
+                            Backline Provided:
+                            <strong>{{
+                                event.additional_data?.backline_provided
+                                    ? "Yes"
+                                    : "No"
+                            }}</strong>
+                        </li>
+                        <li>
+                            Production Needed:
+                            <strong>{{
+                                event.additional_data?.production_needed
+                                    ? "Yes"
+                                    : "No"
+                            }}</strong>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li
+                v-if="
+                    event.event_type_id === 1 && event.additional_data?.wedding
+                "
+                class="p-2"
+            >
+                Wedding Info:
+                <Wedding :wedding="event.additional_data?.wedding" />
+            </li>
+            <li v-if="event.additional_data?.attire">
+                Attire:
+                <div
+                    class="ml-3 p-3 shadow-lg rounded break-normal bg-gray-100 dark:bg-slate-700"
+                    v-html="event.additional_data?.attire"
+                />
+            </li>
+            <Contacts :contacts="event.contacts" />
         </ul>
-        </div>
-      </li>
-      <li
-        v-if="event.event_type_id === 1 && event.additional_data?.wedding"
-        class="p-2"
-      >
-        Wedding Info:
-        <Wedding :wedding="event.additional_data?.wedding" />
-      </li>
-      <li v-if="event.additional_data?.attire">
-        Attire: <div
-          class="ml-3 p-3 shadow-lg rounded break-normal bg-gray-100 dark:bg-slate-700"
-          v-html="event.additional_data?.attire"
-        />
-      </li>      
-      <Contacts
-        :contacts="event.contacts"
-      />
-    </ul>
-  </div>
+    </div>
 </template>
 
 <script setup>
-import Times from './Components/Times.vue'
-import Wedding from './Components/Wedding.vue';
-import Contacts from './Components/Contacts.vue';
+import Times from "./Components/Times.vue";
+import Wedding from "./Components/Wedding.vue";
+import Contacts from "./Components/Contacts.vue";
+import { find } from "lodash";
 
-const props = defineProps(['event', 'type']);
-
+const props = defineProps(["event", "type"]);
 </script>
--- resources/js/Pages/Bookings/Components/BookingCreation.vue
+++ resources/js/Pages/Bookings/Components/BookingCreation.vue
@@ -1,184 +1,193 @@
 <template>
-  <div class="max-w-2xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
-    <h1 class="text-2xl font-semibold text-gray-900 mb-6">
-      Create New Booking for {{ band.name }}
-    </h1>
-    <form @submit.prevent="submitForm">
-      <div class="space-y-6">
-        <div>
-          <label
-            for="name"
-            class="block text-sm font-medium text-gray-700 dark:text-gray-50"
-          >Booking Name</label>
-          <input
-            id="name"
-            v-model="form.name"
-            type="text"
-            required
-            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          >
-        </div>
+    <div class="max-w-2xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-semibold text-gray-900 mb-6">
+            Create New Booking for {{ band.name }}
+        </h1>
+        <form @submit.prevent="submitForm">
+            <div class="space-y-6">
+                <div>
+                    <label
+                        for="name"
+                        class="block text-sm font-medium text-gray-700 dark:text-gray-50"
+                        >Booking Name</label
+                    >
+                    <input
+                        id="name"
+                        v-model="form.name"
+                        type="text"
+                        required
+                        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                </div>
 
-        <div>
-          <label
-            for="event_type_id"
-            class="block text-sm font-medium text-gray-700 dark:text-gray-50"
-          >Event Type</label>
-          <select
-            id="event_type_id"
-            v-model="form.event_type_id"
-            required
-            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          >
-            <option value="">
-              Select an event type
-            </option>
-            <option
-              v-for="event in eventTypes"
-              :key="event.id"
-              :value="event.id"
-            >
-              {{ event.name }}
-            </option>
-          </select>
-        </div>
+                <div>
+                    <label
+                        for="event_type_id"
+                        class="block text-sm font-medium text-gray-700 dark:text-gray-50"
+                        >Event Type</label
+                    >
+                    <select
+                        id="event_type_id"
+                        v-model="form.event_type_id"
+                        required
+                        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
+                        <option value="">Select an event type</option>
+                        <option
+                            v-for="event in eventTypes"
+                            :key="event.id"
+                            :value="event.id"
+                        >
+                            {{ event.name }}
+                        </option>
+                    </select>
+                </div>
 
-        <div>
-          <label
-            for="date"
-            class="block text-sm font-medium text-gray-700 dark:text-gray-50"
-          >Date</label>
-          <DatePicker
-            id="date"
-            v-model="bookingDate"
-            class="w-full"
-            :show-icon="true"
-          />
-        </div>
+                <div>
+                    <label
+                        for="date"
+                        class="block text-sm font-medium text-gray-700 dark:text-gray-50"
+                        >Date</label
+                    >
+                    <DatePicker
+                        id="date"
+                        v-model="bookingDate"
+                        class="w-full"
+                        :show-icon="true"
+                    />
 
-        <div>
-          <label
-            for="start_time"
-            class="block text-sm font-medium text-gray-700 dark:text-gray-50"
-          >Start Time</label>
-          <input
-            id="start_time"
-            v-model="form.start_time"
-            type="time"
-            required
-            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          >
-        </div>
+                    <reserved-calendar :booked-dates="bookedDates" />
+                </div>
 
-        <div>
-          <label
-            for="duration"
-            class="block text-sm font-medium text-gray-700 dark:text-gray-50"
-          >Duration (hours)</label>
-          <input
-            id="duration"
-            v-model="form.duration"
-            type="number"
-            required
-            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          >
-        </div>
+                <div>
+                    <label
+                        for="start_time"
+                        class="block text-sm font-medium text-gray-700 dark:text-gray-50"
+                        >Start Time</label
+                    >
+                    <input
+                        id="start_time"
+                        v-model="form.start_time"
+                        type="time"
+                        required
+                        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                </div>
 
-        <div>
-          <label
-            for="price"
-            class="block text-sm font-medium text-gray-700 dark:text-gray-50"
-          >Price</label>
-          <div class="mt-1 relative rounded-md shadow-sm">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <span class="text-gray-500 sm:text-sm">$</span>
+                <div>
+                    <label
+                        for="duration"
+                        class="block text-sm font-medium text-gray-700 dark:text-gray-50"
+                        >Duration (hours)</label
+                    >
+                    <input
+                        id="duration"
+                        v-model="form.duration"
+                        type="number"
+                        required
+                        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                </div>
+
+                <div>
+                    <label
+                        for="price"
+                        class="block text-sm font-medium text-gray-700 dark:text-gray-50"
+                        >Price</label
+                    >
+                    <div class="mt-1 relative rounded-md shadow-sm">
+                        <div
+                            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+                        >
+                            <span class="text-gray-500 sm:text-sm">$</span>
+                        </div>
+
+                        <InputNumber
+                            id="price"
+                            v-model="form.price"
+                            mode="currency"
+                            currency="USD"
+                            locale="en-US"
+                            class="w-full rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                    </div>
+                </div>
+                <ContractOptions v-model="form.contract_option" />
+                <div>
+                    <button
+                        type="submit"
+                        class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Create Booking
+                    </button>
+                </div>
             </div>
-
-            <InputNumber
-              id="price"
-              v-model="form.price"
-              mode="currency"
-              currency="USD"
-              locale="en-US"
-              class="w-full rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-        </div>
-        <ContractOptions v-model="form.contract_option" />
-        <div>
-          <button
-            type="submit"
-            class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Create Booking
-          </button>
-        </div>
-      </div>
-    </form>
-  </div>
+        </form>
+    </div>
 </template>
 
-  <script>
-  import { ref } from 'vue'
-  import ContractOptions from './ContractOptions.vue'
-  import { useForm } from '@inertiajs/vue3'
-  import DatePicker from 'primevue/datepicker'
-  import { DateTime } from 'luxon';
+<script>
+import { ref } from "vue";
+import ContractOptions from "./ContractOptions.vue";
+import { useForm } from "@inertiajs/vue3";
+import DatePicker from "primevue/datepicker";
+import { DateTime } from "luxon";
+import ReservedCalendar from "@/Components/ReservedCalendar.vue";
 
-
-  export default {
+export default {
     components: {
-      ContractOptions,
-      DatePicker
+        ContractOptions,
+        DatePicker,
     },
     props: {
-      band: {
-        type: Object,
-        required: true
-      },
-      eventTypes: {
-        type: Array,
-        required: true
-      }
+        band: {
+            type: Object,
+            required: true,
+        },
+        eventTypes: {
+            type: Array,
+            required: true,
+        },
+        bookedDates: {
+            type: Array,
+            required: true,
+        },
     },
     setup(props) {
-      const form = useForm({
-        name: '',
-        event_type_id: '',
-        date: '',
-        start_time: '19:00',
-        duration: 4,
-        price: null,
-        contract_option: ''
-      })
+        const form = useForm({
+            name: "",
+            event_type_id: "",
+            date: "",
+            start_time: "19:00",
+            duration: 4,
+            price: null,
+            contract_option: "",
+        });
 
-      //primevue datepicker uses a model of type Date
-      //so when submitting, we need to convert the date to an ISODate
-      const bookingDate = ref(new Date())
+        //primevue datepicker uses a model of type Date
+        //so when submitting, we need to convert the date to an ISODate
+        const bookingDate = ref(new Date());
 
+        const submitForm = () => {
+            form.post(route("bands.booking.store", props.band.id), {
+                preserveScroll: true,
+                preserveState: true,
+            });
+        };
 
-
-      const submitForm = () => {
-
-        form.post(route('bands.booking.store', props.band.id), {
-          preserveScroll: true,
-          preserveState: true
-        })
-      }
-
-      return {
-        form,
-        bookingDate,
-        submitForm
-      }
+        return {
+            form,
+            bookingDate,
+            submitForm,
+        };
     },
     watch: {
-      bookingDate: {
-        immediate: true,
-        handler(newDate) {
-          this.form.date = DateTime.fromJSDate(newDate).toISODate()
-        }
-      }
-    }
-  }
-  </script>
+        bookingDate: {
+            immediate: true,
+            handler(newDate) {
+                this.form.date = DateTime.fromJSDate(newDate).toISODate();
+            },
+        },
+    },
+};
+</script>
