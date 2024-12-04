83120ddac4a67ff85870c80908d9dee4464ce2b6
eddimull <eddimull@gmail.com>
2024-12-04 15:09:16 -0600

    fixed nav on mobile notification colors

--- resources/js/Components/NotificationDropdown.vue
+++ resources/js/Components/NotificationDropdown.vue
@@ -1,30 +1,33 @@
 <template>
-    <Link :class="[
-                {
-                    'bg-blue-200':unread,
-                    'hover:bg-blue-300':unread,
-                    'hover:bg-gray-100':!unread,
-                },
-                'block', 
-                'w-full',
-                'px-4', 
-                'py-2', 
-                'text-left', 
-                'text-sm', 
-                'leading-5', 
-                'text-gray-700', 
-                'focus:outline-none',
-                'focus:bg-gray-100',
-                'transition duration-150',
-                'ease-in-out']"
-                preserve-state
-                >
-
+    <Link
+        :class="[
+            {
+                'bg-blue-200 dark:bg-blue-800': unread,
+                'hover:bg-blue-300 dark:hover:bg-blue-900': unread,
+                'hover:bg-gray-100 dark:hover:bg-gray-700': !unread,
+            },
+            'block',
+            'w-full',
+            'px-4',
+            'py-2',
+            'text-left',
+            'text-sm',
+            'leading-5',
+            'text-gray-700',
+            'dark:text-gray-300',
+            'focus:outline-none',
+            'focus:bg-gray-100',
+            'dark:focus:bg-gray-700',
+            'transition duration-150',
+            'ease-in-out',
+        ]"
+        preserve-state
+    >
         <slot />
     </Link>
 </template>
 <script>
-    export default {
-        props:['unread']
-    }
+export default {
+    props: ["unread"],
+};
 </script>
