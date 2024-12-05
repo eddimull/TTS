18dde69166c8ac1ffeba18d06e91217a747056af
eddimull <eddimull@gmail.com>
2024-12-05 16:07:02 -0600

    can delete payments

--- resources/js/Pages/Bookings/Components/PaymentActions.vue
+++ resources/js/Pages/Bookings/Components/PaymentActions.vue
@@ -1,38 +1,37 @@
 <template>
-  <div>
-    <div
-      class="flex flex-wrap content-around justify-evenly lg:-mb-4"
-    >
-      <Button
-        v-if="booking.amountLeft >= '0.00'"
-        icon="pi pi-dollar"
-        label="Make Payment"
-        class="p-button-success"
-        @click="$emit('openDialog')"
-      />
-      <Button
-        v-if="booking.amountLeft >= '0.00'"
-        icon="pi pi-receipt"
-        label="Create Invoice"
-        class="p-button-success"
-        @click="$emit('openInvoiceDialog')"
-      />
-      <Button
-        icon="pi pi-download"
-        label="Download Receipt"
-        class="p-button-default"
-        @click="$emit('downloadReceipt')"
-      />
+    <div>
+        <div
+            class="flex flex-wrap content-around justify-evenly lg:-mb-4 gap-y-8"
+        >
+            <Button
+                v-if="booking.amountLeft >= '0.00'"
+                icon="pi pi-dollar"
+                label="Make Payment"
+                class="grow lg:grow-0"
+                @click="$emit('openDialog')"
+            />
+            <Button
+                v-if="booking.amountLeft >= '0.00'"
+                icon="pi pi-receipt"
+                label="Create Invoice"
+                class="grow lg:grow-0"
+                @click="$emit('openInvoiceDialog')"
+            />
+            <Button
+                icon="pi pi-download"
+                label="Download Receipt"
+                class="grow lg:grow-0"
+                @click="$emit('downloadReceipt')"
+            />
+        </div>
     </div>
-  </div>
 </template>
 <script setup>
-    defineEmits(['openDialog', 'downloadReceipt', 'openInvoiceDialog']);
-    const props = defineProps({
-        booking: {
-            type: Object,
-            required: true
-        },
-    });
-
+defineEmits(["openDialog", "downloadReceipt", "openInvoiceDialog"]);
+const props = defineProps({
+    booking: {
+        type: Object,
+        required: true,
+    },
+});
 </script>
--- resources/js/Pages/Bookings/Components/PaymentList.vue
+++ resources/js/Pages/Bookings/Components/PaymentList.vue
@@ -1,28 +1,32 @@
 <template>
-  <div>
-    <div
-      v-if="payments.length === 0"
-      class="mb-6 py-6 border-t border-b border-gray-200"
-    >
-      <span class="bold">NO PAYMENTS HAVE BEEN RECEIVED</span>
+    <div>
+        <div
+            v-if="payments.length === 0"
+            class="mb-6 py-6 border-t border-b border-gray-200"
+        >
+            <span class="bold">NO PAYMENTS HAVE BEEN RECEIVED</span>
+        </div>
+        <PaymentItem
+            v-for="payment in payments"
+            :key="payment.id"
+            class="mb-6 py-6 border-t border-b border-gray-200 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600"
+            :payment="payment"
+            :booking="booking"
+            @dblclick="payment.enableDelete = !payment.enableDelete"
+        />
     </div>
-    <PaymentItem
-      v-for="payment in payments"
-      :key="payment.id"
-      class="mb-6 py-6 border-t border-b border-gray-200 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600"
-      :payment="payment"
-      @dblclick="payment.enableDelete = !payment.enableDelete"
-    />       
-  </div>
 </template>
 <script setup>
-import PaymentItem from './PaymentItem.vue';
+import PaymentItem from "./PaymentItem.vue";
 
 const props = defineProps({
-  payments: {
-    type: Object,
-    required: true
-  }
+    payments: {
+        type: Object,
+        required: true,
+    },
+    booking: {
+        type: Object,
+        required: true,
+    },
 });
-
 </script>
