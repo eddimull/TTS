c950a893671a43476ba64373ea3c1e0d057d442b
eddimull <eddimull@gmail.com>
2025-10-25 18:48:55 -0500

    removing padding on sm screens

--- resources/js/Components/Container.vue
+++ resources/js/Components/Container.vue
@@ -1,7 +1,7 @@
 <template>
-    <div
-        class="container mx-auto px-0.5 md:px-4 bg-white dark:bg-slate-600 rounded-none sm:rounded-md"
-    >
-        <slot />
-    </div>
+  <div
+    class="container mx-auto px-0 bg-white dark:bg-slate-600 rounded-none sm:rounded-md"
+  >
+    <slot />
+  </div>
 </template>
