049afd022563df34ce2cc954a182de8fe0e7f5b8
eddimull <eddimull@gmail.com>
2026-02-11 21:17:16 -0600

    so many logic flaws in the gcal queue

--- app/Observers/EventObserver.php
+++ app/Observers/EventObserver.php
@@ -12,13 +12,18 @@ class EventObserver
     public function created(Events $event)
     {
         \Log::info('Event created observer triggered for event ID: ' . $event->id);
+        \Log::debug('Dispatching ProcessEventCreated job for event ID: ' . $event->id);
         ProcessEventCreated::dispatch($event);
     }
 
     public function updated(Events $event)
     {
         \Log::info('Event updated observer triggered for event ID: ' . $event->id);
-        ProcessEventUpdated::dispatch($event, $event->getOriginal());
+        \Log::debug('Dispatching ProcessEventUpdated job for event ID: ' . $event->id);
+
+        // Delay job by 2 seconds to allow rapid successive updates to settle
+        // This batches rapid edits so the job processes the final state
+        ProcessEventUpdated::dispatch($event, $event->getOriginal())->delay(now()->addSeconds(2));
     }
 
     public function deleted(Events $event)
