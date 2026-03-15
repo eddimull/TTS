2a9c156f2b98f823651c658c8cc9c0c89f972a7b
eddimull <eddimull@gmail.com>
2026-03-15 17:52:18 -0500

    enummmm

---
+++ app/Enums/BandResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Enums;
+
+enum BandResource: string
+{
+    case Events = 'events';
+    case Proposals = 'proposals';
+    case Invoices = 'invoices';
+    case Colors = 'colors';
+    case Charts = 'charts';
+    case Bookings = 'bookings';
+    case Rehearsals = 'rehearsals';
+    case Media = 'media';
+
+    public function readPermission(): string
+    {
+        return 'read:' . $this->value;
+    }
+
+    public function writePermission(): string
+    {
+        return 'write:' . $this->value;
+    }
+
+    public function label(): string
+    {
+        return ucfirst($this->value);
+    }
+}
